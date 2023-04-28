76818bd9901788bd9a3f3b1f925921fd6dab0531
Hariharan2293 <112700316+Hariharan2293@users.noreply.github.com>
2023-04-28 08:55:16 +0530

    Add files via upload

---
+++ simpcalc.py
@@ -0,0 +1,12 @@
+a=int(input("Enter:"));
+b=int(input("Enter:"));
+c=input("Enter operation:");
+if(c=='+'):
+    print(a+b);
+if(c=='-'):
+    print(a-b);
+if(c=='*' or c=='X'):
+    print(a*b);
+if(c=='%'):
+    print(a%b);
+    
